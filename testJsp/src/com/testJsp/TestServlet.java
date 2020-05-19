1d44074c70d4b749e44a80fbc8ae6356a7ca7396
Ho <ii8858@naver.com>
2020-05-19 19:38:33 +0900

    restart

---
+++ testJsp/src/com/testJsp/TestServlet.java
@@ -0,0 +1,68 @@
+package com.testJsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TS1")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("---doGet()---");
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<head>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<p>Hello Servlet!!</p>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public void funPc() {
+		System.out.println("----@PostCoinstruct-----");
+	
+	}
+	public void init() throws ServletException {
+		System.out.println("----init()-----");
+	}
+	
+	public void destroy() {
+		System.out.println("----destroy()----");
+	}
+	public void funPd() {
+		System.out.println("----@PreDestroy-----");
+	
+	}
+
+}
