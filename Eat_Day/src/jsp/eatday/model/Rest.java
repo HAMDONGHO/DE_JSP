0dd9fda4372f112eeeee52d400510ba2ecf3e530
Ho <ii8858@naver.com>
2020-07-01 10:37:22 +0900

    connect DB

--- Eat_Day/src/jsp/eatday/model/Rest.java
+++ Eat_Day/src/jsp/eatday/model/Rest.java
@@ -9,7 +9,7 @@ public class Rest {
 	private String salestime;
 	private String classfy;
 	private String weather;
-	private String temperature;
+	private int temperature;
 	
 	public String getId() {
 		return id;
@@ -59,10 +59,10 @@ public class Rest {
 	public void setWeather(String weather) {
 		this.weather = weather;
 	}
-	public String getTemperature() {
+	public int getTemperature() {
 		return temperature;
 	}
-	public void setTemperature(String temperature) {
+	public void setTemperature(int temperature) {
 		this.temperature = temperature;
 	}
 	
